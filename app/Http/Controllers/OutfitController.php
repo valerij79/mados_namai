e4c851f421fcfd5aae134158886565349b9df24e
valerij79 <tumkevich@gmail.com>
2023-05-29 14:51:24 +0300

    Register, login and Models

---
+++ app/Http/Controllers/OutfitController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Outfit;
+use App\Http\Requests\StoreOutfitRequest;
+use App\Http\Requests\UpdateOutfitRequest;
+
+class OutfitController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreOutfitRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Outfit $outfit)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Outfit $outfit)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateOutfitRequest $request, Outfit $outfit)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Outfit $outfit)
+    {
+        //
+    }
+}
